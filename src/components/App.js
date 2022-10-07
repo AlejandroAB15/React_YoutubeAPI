9689cd1acfc576aa4717e397ac49fec1f157c6bb
Alejandro Alonso <alejandroab5147@gmail.com>
2022-10-07 18:37:42 -0500

    add basic setup for the app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+
+    render(){
+        return(
+            <div className="container">
+                <SearchBar/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        //If a function from the parent element is sent as a prop the child element can make use of it 
+        this.props.onSubmit(this.state.term);
+    }
+    
+    //We reassing the value of the input so the whole information is driven by the React App and not by the HTML part of the project
+    render() {
+        return(
+            <div className="field" style={{marginTop: "1rem"}}>
+                    <form onSubmit={this.onFormSubmit}>
+                        <p className="control has-icons-right">
+                            <input type="text" className="input" placeholder="Video Search"
+                             onChange={ e => { this.setState({ term: e.target.value }) } }
+                             value={this.state.term}/>
+                            <span className="icon is-small is-right">
+                                <i className="fas fa-solid fa-magnifying-glass"></i>
+                            </span>
+                        </p>
+                    </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
