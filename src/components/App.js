18ca386a399cf49ad66f4805133fa56ecaba4f22
Alejandro Alonso <alejandroab5147@gmail.com>
2022-10-07 19:39:00 -0500

    add API search data relationship between component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../API/youtube";
 
 class App extends React.Component {
+    state = { videos: [] }
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+        
+        this.setState({ videos: response.data.items });
+    }
 
     render(){
         return(
             <div className="container">
-                <SearchBar/>
+                <SearchBar onTermSubmit = {this.onTermSubmit}/>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component{
     onFormSubmit = (event) => {
         event.preventDefault();
         //If a function from the parent element is sent as a prop the child element can make use of it 
-        this.props.onSubmit(this.state.term);
+        this.props.onTermSubmit(this.state.term);
     }
     
     //We reassing the value of the input so the whole information is driven by the React App and not by the HTML part of the project
