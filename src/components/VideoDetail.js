67b40b9d8bccd87c034bd913107b568afd293baf
Alejandro Alonso <alejandroab5147@gmail.com>
2022-10-08 20:00:44 -0500

    add video player to VideoDetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,9 +4,14 @@ const VideoDetail = ({ video }) => {
     if(!video) {
         return <div>Loading...</div>
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     
     return(
         <div className="box">
+            <figure className="image is-16by9"  style={{marginBottom: "1rem"}} >
+                <iframe src={videoSrc} className = "has-ratio" title="video"/>
+            </figure>
             <h5 className="title is-5">{video.snippet.title}</h5>
             <div className="content">
                 <p>{video.snippet.description}</p>
